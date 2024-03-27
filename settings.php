798e701cc01ffb314f3e1073de3136bbc21f3ccf
kerogs <luckywies@gmail.com>
2024-03-27 12:38:04 +0100

    1.1.3-alpha

---
+++ settings.php
@@ -0,0 +1,34 @@
+
+<?php
+
+    require_once './src/php/session.php';
+ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sword Master Story</title>
+    <link rel="stylesheet" href="./src/css/style.css">
+
+    <?php require_once './src/php/head.php' ?>
+</head>
+
+<body>
+
+    <main>
+
+        <!-- navigation -->
+        <?php
+        
+            require_once './src/php/header.php';
+
+        ?>
+
+    </main>
+
+</body>
+
+</html>
---
+++ src/php/head.php
@@ -0,0 +1,5 @@
+<!-- theme color -->
+<meta name="theme-color" content="#2e2a79">
+
+<!-- favicon -->
+<link rel="shortcut icon" href="./src/img/icon.png" type="image/x-icon">
